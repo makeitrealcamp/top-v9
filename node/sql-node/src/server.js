3d813dac795adf85fc8bf72733f0d3633bc7a0cd
Simon Hoyos <shmesa23@gmail.com>
2021-07-28 10:54:25 -0500

    Sequelize first steps

---
+++ node/sql-node/src/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const { sequelize } = require('./models')
+
+const port = process.env.PORT || 8000
+const app = express()
+sequelize.sync()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`)
+});

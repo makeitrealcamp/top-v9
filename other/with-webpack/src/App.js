673f7b3bb432a398784c3f05f36363b934e163d1
Simon Hoyos <shmesa23@gmail.com>
2021-08-12 11:26:15 -0500

    Webpack

---
+++ other/with-webpack/src/App.js
@@ -0,0 +1,9 @@
+import './App.scss'
+
+function App() {
+  return (
+    <h1 className="title">Hola mundo</h1>
+  )
+}
+
+export default App
---
+++ other/with-webpack/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-react',
+              {
+                runtime: 'automatic'
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ]
+}
